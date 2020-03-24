7c0de336933a6d26f24add1762fd3f4cfe12ba61
chrisnoel485 <chrisnoel485@gmail.com>
2020-03-24 18:50:29 +0700

    add server warranty -c

--- app/Server.php
+++ app/Server.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class warranty extends Model
+class Server extends Model
 {
     //
 }
--- app/Warranty.php
+++ app/Warranty.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class server extends Model
+class Warranty extends Model
 {
     //
 }
