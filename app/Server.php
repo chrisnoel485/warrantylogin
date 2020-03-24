3e30cf74764637799f3ef78593a6ba173152b887
chrisnoel485 <chrisnoel485@gmail.com>
2020-03-25 00:02:36 +0700

    add model server -m

--- app/Server.php
+++ app/Server.php
@@ -7,10 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Server extends Model
 {
     //
-    protected $table = "servers";
- 
-    public function warranty()
-    {
-    	return $this->hasOne('App\Warranty');
-    }
 }
