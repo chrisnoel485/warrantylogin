2fe5cd49bf5f25035575deb9aa23f0fd6d4db48a
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-03-24 22:49:08 +0700

    add server warranty -cmr

--- resources/views/server.blade.php
+++ resources/views/server.blade.php
@@ -1,22 +1,27 @@
-@extends('adminlte::page')
-@section('content')
-<div class="container">
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel #23 : Relasi One To One Eloquent</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+ 
+	<div class="container">
 		<div class="card mt-5">
 			<div class="card-body">
-				<h5 class="text-center my-4">Daftar Warranty</h5>
+				<h3 class="text-center"><a href="https://www.malasngoding.com">www.malasngoding.com</a></h3>
+				<h5 class="text-center my-4">Eloquent One To One Relationship</h5>
 				<table class="table table-bordered table-striped">
 					<thead>
 						<tr>
-							<th>Serial Number</th>
 							<th>Hostname</th>
-                            <th>Tahun Pembelian</th>
-                            <th>Expired Warranty</th>
+							<th>tahun pembelian</th>
 						</tr>
 					</thead>
 					<tbody>
 						@foreach($server as $p)
 						<tr>
-                            <td>{{ $p->hostname }}</td>
+							<td>{{ $p->hostname }}</td>
 							<td>{{ $p->warranty->tahun_pembelian }}</td>
 						</tr>
 						@endforeach
@@ -25,8 +30,6 @@
 			</div>
 		</div>
 	</div>
-@endsection
-
-@section('css')
-  <link rel="stylesheet" href="{{asset('css/app.css')}}">
-@endsection
+ 
+</body>
+</html>
