edb1f2f8d5325860124eee60df303c43be1ad414
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-03-24 21:12:55 +0700

    add server warranty -cmr

---
+++ app/Http/Controllers/ServerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Server;
+
+class ServerController extends Controller
+{
+    //
+    public function index()
+    {
+    	$server = Server::all();
+        return view('warranty/home', ['server' => $server]);
+    }
+}
