cbcc6f19b53c3df8f25963f8195d4b51697b9171
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-03-24 09:03:42 +0700

    add server logo

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class ProfileController extends Controller
 {
@@ -14,6 +15,8 @@ class ProfileController extends Controller
     public function index()
     {
         //
+        $user = User::all();
+        return view ('admin/profile', compct('user'));
     }
 
     /**
--- config/adminlte.php
+++ config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => 'Warranty Management',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => '<b>Warranty</b>Management',
+    'logo' => '<b>Warranty</b>MGMT',
     'logo_img' => 'vendor/adminlte/dist/img/ServerLogo.jpg',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
