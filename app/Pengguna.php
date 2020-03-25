a59301f52f83b81a2655bed17e41f7332f3cff53
chrisnoel485 <chrisnoel485@gmail.com>
2020-03-25 16:55:43 +0700

    1

--- app/Pengguna.php
+++ app/Pengguna.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class telepon extends Model
+class Pengguna extends Model
 {
     //
 }
--- app/Telepon.php
+++ app/Telepon.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class pengguna extends Model
+class Telepon extends Model
 {
     //
 }
