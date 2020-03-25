4ea39c1b1ecd35908693f8f2c1fbe91d281a2dc9
chrisnoel485 <chrisnoel485@gmail.com>
2020-03-25 10:15:31 +0700

    delete

--- app/Warranty.php
+++ app/Warranty.php
@@ -7,10 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Warranty extends Model
 {
     //
-    protected $table = "warranties";
- 
-    public function server()
-    {
-    	return $this->belongsTo('App\Server');
-    }
 }
