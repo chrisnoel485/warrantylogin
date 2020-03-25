b1a49c779e7bdb1052bc128fa1601343c1ec2786
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-03-25 07:47:12 +0700

    add migration server and warranty

--- database/migrations/2020_03_25_003202_create_servers_table.php
+++ database/migrations/2020_03_25_003202_create_servers_table.php
@@ -14,7 +14,7 @@ class CreateServersTable extends Migration
     public function up()
     {
         Schema::create('servers', function (Blueprint $table) {
-            $table->integer('id');
+            $table->increments('id');
             $table->string('hostname');
             $table->timestamps();
         });
