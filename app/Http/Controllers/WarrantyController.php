1c1954322738a81faa332f35edd726c5fc5e341a
chrisnoel485 <51897833+chrisnoel485@users.noreply.github.com>
2020-03-25 09:54:21 +0700

    fix server and warranty

--- app/Http/Controllers/WarrantyController.php
+++ app/Http/Controllers/WarrantyController.php
@@ -18,7 +18,7 @@ class ServerController extends Controller
         //
         $server = Server::all();
         //$warranty = Warranty::all();
-        return view('server/warranty',['server' => $server],['warranty' => $warranty]);
+        return view('server/warranty',['server' => $server]);
         //return view('server/warranty',compact('servers','warranties','server','warranty'));
     }
 
